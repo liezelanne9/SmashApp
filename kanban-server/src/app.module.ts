270c879d5d7ba724cab94d3396d6d3f468ca7ef9
AndrewUrteaga <aurte002@ucr.edu>
2019-03-23 18:06:24 -0700

    first frontend

--- kanban-server/src/app.module.ts
+++ kanban-server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NavBarComponent } from './nav/navbar.component';
 
 @Module({
   imports: [],
+  components: [
+    NavBarComponent,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ kanban-server/src/nav/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'nav-bar',
+    templateURL: './navbar.component.html',
+})
+export class NavBarComponent {
+}
